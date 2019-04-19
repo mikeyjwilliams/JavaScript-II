61450d5f3a7e5820a27e872afd544f68b9f140c3
mikey <manicdesigner@gmail.com>
2019-04-19 18:16:35 -0400

    feat(callback) sumNums add to numbers in callback

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -36,13 +36,15 @@ function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr.splice(last, 1));
 }
-last(items, function (lastItem) {
-  console.log(lastItem);
-});
+// last(items, function (lastItem) {
+//   console.log(lastItem);
+// });
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
